27cf0c3219d6fc3bc54aa15b005a8b9f9790e1a4
Paritosh Fulsunge <paritosh.fulsunge@gmail.com>
2023-09-01 15:35:36 -0700

    fixing the typo in app name

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import LayoutFlow from './AppFlow'
+import AppFlow from './AppFlow'
 import './index.css'
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <LayoutFlow />
+    <AppFlow />
   </React.StrictMode>,
 )
